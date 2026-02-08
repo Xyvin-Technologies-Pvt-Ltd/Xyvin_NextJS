e09546e49248e05d53f4a6538fff78b69d6ffb92
fasilfaz <fazsby2001@gmail.com>
2026-02-08 14:50:30 +0530

    team member updated

--- src/data/team_data.ts
+++ src/data/team_data.ts
@@ -3,7 +3,7 @@ interface DataType {
 	img: string;
 	avatar_name: string;
 	designation: string;
-	linkedin:string
+	linkedin: string
 }
 
 const team_data: DataType[] = [
@@ -12,14 +12,14 @@ const team_data: DataType[] = [
 		img: "/assets/img/team/Team_2.png",
 		avatar_name: "Ashin Amanulla",
 		designation: "Chief Executive Officer",
-		linkedin:"https://www.linkedin.com/in/ashin-amanulla/"
+		linkedin: "https://www.linkedin.com/in/ashin-amanulla/"
 	},
 	{
 		id: 1,
 		img: "/assets/img/team/Team_1.png",
 		avatar_name: "Adithya Vishnu",
 		designation: "Chief Product Officer",
-		linkedin:"https://www.linkedin.com/in/adithya-vishnu-tm-218b501b4/"
+		linkedin: "https://www.linkedin.com/in/adithya-vishnu-tm-218b501b4/"
 	},
 
 	{
@@ -27,36 +27,36 @@ const team_data: DataType[] = [
 		img: "/assets/img/team/Team_3.png",
 		avatar_name: "Afsal E",
 		designation: "Chief Business Officer",
-		linkedin:"https://www.linkedin.com/in/afsal-e-muhammed-66998b1b1/"
+		linkedin: "https://www.linkedin.com/in/afsal-e-muhammed-66998b1b1/"
 	},
 	{
 		id: 4,
 		img: "/assets/img/team/Team_4.png",
 		avatar_name: "Ramees",
 		designation: "Chief Marketing Officer",
-		linkedin:"https://www.linkedin.com/in/ramees-a-b379b7247/"
+		linkedin: "https://www.linkedin.com/in/ramees-a-b379b7247/"
 	},
-	{
-		id: 5,
-		img: "/assets/img/team/Frame 1171276608.png",
-		avatar_name: "Tijo Joseph",
-		designation: "Tech Alchemist",
-		linkedin:"https://in.linkedin.com/in/tijo-t-joseph"
-	},
-	{
-		id: 6,
-		img: "/assets/img/team/Frame 1171276607.png",
-		avatar_name: "Anjana",
-		designation: "Pixel Perfectionist",
-		linkedin:"https://in.linkedin.com/in/anjana-vu-dev"
-	},
-	{
-		id: 7,
-		img: "/assets/img/team/Frame 1171276605.png",
-		avatar_name: "Sreeram",
-		designation: "App Developer",
-		linkedin:"https://www.linkedin.com/in/sreeram-i-s-37403a204?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app"
-	},
+	// {
+	// 	id: 5,
+	// 	img: "/assets/img/team/Frame 1171276608.png",
+	// 	avatar_name: "Tijo Joseph",
+	// 	designation: "Tech Alchemist",
+	// 	linkedin:"https://in.linkedin.com/in/tijo-t-joseph"
+	// },
+	// {
+	// 	id: 6,
+	// 	img: "/assets/img/team/Frame 1171276607.png",
+	// 	avatar_name: "Anjana",
+	// 	designation: "Pixel Perfectionist",
+	// 	linkedin:"https://in.linkedin.com/in/anjana-vu-dev"
+	// },
+	// {
+	// 	id: 7,
+	// 	img: "/assets/img/team/Frame 1171276605.png",
+	// 	avatar_name: "Sreeram",
+	// 	designation: "App Developer",
+	// 	linkedin:"https://www.linkedin.com/in/sreeram-i-s-37403a204?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app"
+	// },
 	// {
 	// 	id: 8,
 	// 	img: "/assets/img/team/Frame 1171276606.png",
